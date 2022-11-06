75acb240b8617545957788fcd652a6e359c4ccc3
ThiagoGlady <thiagoglady@gmail.com>
2022-11-06 00:14:44 -0300

    criado os botões que levam até a página de criação aleatória e criação configurada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+    redirectToRandomSheet = () => {
+        const { history } = this.props;
+        history.push('/sheet/random');
+    }
+
+    redirectToSetSheet = () => {
+        const { history } = this.props;
+        history.push('/sheet/settings');
+    }
+
     render() {
         return (
         <div>
-            Home
+            <div>
+                Deseja deixar o computador criar a sua ficha?
+            </div>
+            <div>
+                <button
+                    type="button"
+                    onClick={ this.redirectToRandomSheet }
+                >
+                    Sim, eu só me preocupo com o ND
+                </button>
+                <button
+                    type="button"
+                    onClick={ this.redirectToSetSheet }
+                >
+                    Não, eu quero selecionar alguns detalhes
+                </button>
+            </div>
         </div>
         );
     }
-} 
+}
+
+Home.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+};
